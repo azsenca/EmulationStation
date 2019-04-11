60e5a319df3f849c3dd1a18c2390fdf1e05e8914
azsenca <hrvoje555@gmail.com>
2019-04-11 18:14:56 +0200

    Update GuiDetectDevice.cpp

--- es-core/src/guis/GuiDetectDevice.cpp
+++ es-core/src/guis/GuiDetectDevice.cpp
@@ -38,7 +38,7 @@ GuiDetectDevice::GuiDetectDevice(Window* window, bool firstRun, const std::funct
 	mGrid.setEntry(mDeviceInfo, Vector2i(0, 1), false, true);
 
 	// message
-	mMsg1 = std::make_shared<TextComponent>(mWindow, "HOLD A BUTTON ON YOUR DEVICE TO CONFIGURE IT.", Font::get(FONT_SIZE_SMALL), 0x777777FF, ALIGN_CENTER);
+	mMsg1 = std::make_shared<TextComponent>(mWindow, "HOLD O BUTTON ON YOUR DEVICE TO CONFIGURE IT.", Font::get(FONT_SIZE_SMALL), 0x777777FF, ALIGN_CENTER);
 	mGrid.setEntry(mMsg1, Vector2i(0, 2), false, true);
 	
 	const char* msg2str = firstRun ? "" : "";
